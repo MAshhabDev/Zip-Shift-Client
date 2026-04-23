2748f3d3adab5bc33f38a440d22d742d3f4c9ab7
Mahir4098 <mahirjr98@gmail.com>
2026-04-23 14:03:00 +0600

    Implement and Connect Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { router } from './Routes/router.jsx'
 import { RouterProvider } from 'react-router'
+import AuthProvider from './Context/AuthContext/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AuthProvider>
     <RouterProvider router={router} />
-  </StrictMode>
+
+  </AuthProvider>
 )
